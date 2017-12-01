e26a515ef946a369cb0104fc6c5b97996097b87e
Christian Lee <hyjim900@gmail.com>
2017-12-01 13:06:53 -0800

    edited adq1 demo

--- matlab_toolbox/Algorithms/ADQ1/demo.m
+++ matlab_toolbox/Algorithms/ADQ1/demo.m
@@ -3,15 +3,54 @@
 % 6th Km Harilaou-Thermis, Thessaloniki 57001, Greece
 
 close all; clear all;
-im1='demo.jpg'
+im1='/Users/christianlee/Downloads/ForensicsProjectImages-20171129/monk_base.jpg';
+im2='/Users/christianlee/Downloads/ForensicsProjectImages-20171129/monk_manip.jpg';
 subplot(2,2,1);
 imshow(CleanUpImage(im1));
+subplot(2,2,2);
+imshow(CleanUpImage(im2));
 subplot(2,2,3);
-[OutputMap, Feature_Vector, coeffArray] = analyze(im1);
+[OutputMap, ~, ~] = analyze(im1);
 imagesc(OutputMap);
-title('JPG');
 subplot(2,2,4);
-im2='demo.png'
-[OutputMap, Feature_Vector, coeffArray] = analyze(im2);
+
+[OutputMap2, ~, ~] = analyze(im2);
+
+imagesc(OutputMap2);
+
+OutputMap = OutputMap/max(OutputMap(:));
+OutputMap2 = OutputMap2/max(OutputMap2(:));
+
+OutputMap = imgaussfilt(OutputMap, 2);
+OutputMap2 = imgaussfilt(OutputMap2, 2);
+
+Edge1 = edge(OutputMap);
+Edge2 = edge(OutputMap2);
+figure;
+subplot(2,1,1);
+imshow(Edge1);
+subplot(2,1,2);
+imshow(Edge2);
+
+OutputMap2 = imgaussfilt(OutputMap2, 3);
+figure;
+imagesc(OutputMap2);
+
+image = rgb2gray(imread('/Users/christianlee/Downloads/ForensicsProjectImages-20171129/monk_base.jpg'));
+[x, y] = size(image);
+OutputMap = imresize(OutputMap, [x, y]);
+
+figure;
 imagesc(OutputMap);
-title('PNG');
+m = max(OutputMap(:));
+for i=1:x
+    for j=1:y
+        if OutputMap(i, j) < m*0.5
+            image(i, j) = 0;
+            
+        end
+    end
+end
+
+figure;
+imshow(image);
